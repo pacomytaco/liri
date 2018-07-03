5d6cbb6a0da85950507eb8096dde7626f3df0637
paco <skykayla915@gmail.com>
2018-07-03 17:20:19 -0400

    crying

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.HVXzqrruUd8ZO1by2BmZbIwjj,
+  consumer_secret: process.env.M2mKB42EtUhu13YtOm8pl4MxI12BNma8WTx2Xx2v7MKa8tWcYq,
+  access_token_key: process.env.1012487294652223489-vDTsGFVwnUPntRhC9UtMG0oeyLVWVO,
+  access_token_secret: process.env.RPRvEU2y9YmC3LdHrVZr8QNo025MEdsvyaHZ45kM6m1Nd
+};
+
+exports.spotify = {
+  id: process.env.9d5d7e12218445739e958516de1a9505,
+  secret: process.env.ead5318774734532bfe6c91162f584f0
+};
---
+++ liri.js
@@ -0,0 +1,117 @@
+require("dotenv").config();
+
+var twitter = require('twitter');
+var spotify = require('spotify');
+
+
+var request = require('request');
+var fs = require('file-system');
+
+var keys = require('./keys.js');
+var twitterkeys = new twitter(keys.twitter);
+var spotifykeys = new spotify(keys.spotify);
+
+var nodeArg = process.argv;
+var liriCmnd = process.arch[2];
+
+var liriArg = '';
+for (var i = 3; i<nodeArg.length; i++) {
+    liriArg += nodeArg[i] + ' ';
+}
+
+switch(liriCmnd) {
+    case "my-tweets":
+    getTweets();
+    break;
+
+    case "spotify-this-song":
+    if(liriArg){
+        spotifySong(liriArg);
+    } else{
+        spotifySong("The Sign")
+    }
+    break;
+
+    case "movie-this":
+    if(liriArg){
+        movieOmdb(liriArg)
+    } else {
+        movieOmdb("Mr. Nobody")
+    }
+    break;
+
+    
+
+}//switchcmnd
+
+function getTweets() {
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i=0; i<tweets.statuses.length; i++) {
+                var tweets = tweets.statuses[i].text;
+                console.log("tweet: " + tweets);
+                var tweetDate = tweets.statuses[i].created_at;
+                console.log("date tweeted: " + tweetDate);
+
+                fs.appendFile('log.txt', "@pacomytaco2: " + tweets + "date tweeted: " + tweetDate.substring(0, 14));
+                fs.appendFile('log.txt', "----------------");
+            }
+        }else {
+            console.log(error);
+        }
+    });
+}//function gettweets
+
+function spotifySong(song){
+    spotify.search({ type: 'track', query: song}, function (error, data) {
+        if(!error) {
+            for(var i = 0; i < data.tracks.itmes[i]; i++) {
+            console.log("Artist: " + songData.artists[0].name);
+            console.log("Song Title: " + songData.name);
+            console.log("preview URL: " + songData.preview_url);
+            console.log("Album Title: " + songData.album.name);
+            console.log("----------------");
+
+            fs.appendFile('log.txt', songData.artists[0].name);
+            fs.appendFile('log.txt', songData.name);
+            fs.appendFile('log.txt', songData.preview_url);
+            fs.appendFile('log.txt', songData.album.name);
+            fs.appendFile('log.txt', "-----------------");
+        }
+    } else {
+        console.log('error');
+    }
+    });
+}
+
+//key http://www.omdbapi.com/?i=tt3896198&apikey=12e4f227
+function movieOmdb(movies){
+    var queryUrl = "http://www.omdbapi.com/?t=" + movies + "&y=&plot=short&tomatoes=true&r=json";
+
+    request(queryUrl, function(error, response, body) {
+        if(!error && response.statuseCode === 200) {
+            var movTitle = JSON.parse(body);
+        
+        console.log("Movie Title: " + movie.Title);
+        console.log("Release Year: " + movie.Year);
+        console.log("IMDB Rating: " + movie.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + movie.Ratings[2].Value);
+        console.log("Produced in (country): " + movie.Country);
+        console.log("Language: " + movie.Language);
+        console.log("Plot: " + movie.Plot);
+        console.log("Actors: " + movie.Actors);
+
+
+        fs.appendFile('log.txt' + movie.Title);
+        fs.appendFile('log.txt' + movie.Year);
+        fs.appendFile('log.txt' + movie.imdbRating);
+        fs.appendFile('log.txt' + movie.Ratings[2].Value);
+        fs.appendFile('log.txt' + movie.Country);
+        fs.appendFile('log.txt' + movie.Language);
+        fs.appendFile('log.txt' + movie.Plot);
+        fs.appendFile('log.txt' + movie.Actors);
+        }
+       
+    });
+}
+
